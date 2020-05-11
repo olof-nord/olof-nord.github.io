466120ee40d86de0e8b44625a0d4032ffa86b13c
Olof Nord <olof.nord@tutanota.com>
2020-05-11 23:11:11 +0200

    Add an emtpy routing module in preparation for routing

--- olof-site/src/app/app.module.ts
+++ olof-site/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { HeaderComponent } from '@header/header.component';
 import { ContactComponent } from '@contact/contact.component';
 import { DashboardComponent } from '@dashboard/dashboard.component';
@@ -17,6 +19,7 @@ import { DashboardComponent } from '@dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     NgbModule
   ],
   providers: [],
